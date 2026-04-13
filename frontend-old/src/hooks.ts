59af77c36e8603e97c5e2fe55004d4d49f0bccb6
Sanjay Sah <sanjayonnet1980@gmail.com>
2026-04-13 15:19:24 +0530

    updated it

--- frontend-old/src/hooks.ts
+++
@@ -1,8 +0,0 @@
-import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
-import type { RootState, AppDispatch } from "./store";
-
-// Typed dispatch
-export const useAppDispatch: () => AppDispatch = useDispatch;
-
-// Typed selector
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- frontend/src/components/Sidebar/SidebarHeader.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const SidebarHeader: React.FC = () => (
-  <div className="sidebar-header">
-    <div className="logo">Monthly Expenses ▼</div>
-  </div>
-);
